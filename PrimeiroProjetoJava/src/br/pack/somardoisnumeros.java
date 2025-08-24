d8d87b9911e65999632834bdd14465e96ba8023e
Jpdnos <ramlethalez@gmail.com>
2025-08-24 16:36:40 -0300

    exercicio1

---
+++ PrimeiroProjetoJava/src/br/pack/somardoisnumeros.java
@@ -0,0 +1,18 @@
+package br.pack;
+
+import java.util.*;
+
+class somardoisnumeros {
+     public static Scanner sc = new Scanner(System.in);
+     
+     public static void main (String[] args) {
+    	 int num1, num2, soma;
+    	 System.out.println("Digite um numero: ");
+    	 num1 = sc.nextInt();
+    	 System.out.println("Digite um numero: ");
+    	 num2 = sc.nextInt();
+    	 soma = num1 + num2;
+    	 System.out.println(soma);
+       
+     }
+}
